f5124829e4f39245afaa3d10806864259eb19d13
Eshin Griffith <eshin@me.com>
2021-08-19 08:35:34 -0400

    Mutable Props more type safe

--- src/use/mutableProps.ts
+++ src/use/mutableProps.ts
@@ -1,12 +1,20 @@
 import { UnwrapRef, Ref, computed } from "vue"
-
-export const useMutableProps = <P extends Record<string, any>>(props: P, emit: (event: `update:${keyof P & string}`, option: any) => void) => new Proxy(props, {
+export type MutablePropKeys<P extends string, Q extends string> = 
+  (P extends "value" ? Q extends "input" ? "value" : never : never) |
+  (Q & `update:${P}` extends `update:${infer A}` ? A : never)
+type MutableProps<P extends Record<string, any>, Q extends string> = {
+  [p in MutablePropKeys<keyof P & string, Q>]: Ref<P[p]>
+}
+export const useMutableProps = <P extends Record<string, any>, Q extends string>(props: P, emit: (event: Q, option: any) => void): MutableProps<P,Q> => new Proxy(props, {
   get(target, key, receiver) {
     if(Reflect.has(target, key) && typeof key === "string") return computed({
       get() {
         return Reflect.get(target, key, receiver)
       },
       set(val) {
+        // @ts-ignore Do both
+        if(key === "value") emit("input", val)
+        // @ts-ignore
         emit(`update:${key}`, val)
         return val
       }
