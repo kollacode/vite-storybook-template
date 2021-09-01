010982d52d88c92bf68c97ce57c1adc2c25f1eaa
Eshin Griffith <eshin@me.com>
2021-09-01 07:57:46 -0400

    Added Start filters

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <ElContainer direction="vertical">
   <top-nav/>
   <ElMain>
-    ... And the rest...
+    <startPage/>
   </ElMain>
 </ElContainer>
 </template>
@@ -14,6 +14,7 @@ import {
   ElMain,
 } from 'element-plus';
 import topNav from '@/components/topNav';
+import startPage from '@/views/start';
 export default defineComponent({
 name: 'App',
 data() {
@@ -24,7 +25,8 @@ data() {
   components: {
     ElContainer,
     ElMain,
-    topNav
+    topNav,
+    startPage
   }
 });
 </script>
---
+++ src/views/start/start.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>Events in progress</h1>
+  <ElRow :gutter="20" justify="space-between">
+    <ElCol :span="16">
+      <ElInput
+        v-model="textQuery"
+        placeholder="Search Events in progress"
+        prefix-icon="el-icon-search"
+      >
+      <template #append>
+        <el-button type="primary">Search Events</el-button>
+      </template>
+      </ElInput>
+    </ElCol>
+    <ElCol :span="8">
+      <el-date-picker
+        v-model="dateRange"
+        type="daterange"
+        range-separator="To"
+        start-placeholder="Start date"
+        end-placeholder="End date">
+      </el-date-picker>
+    </ElCol>
+  </ElRow>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import {
+  ElContainer,
+  ElBreadcrumb,
+  ElHeader,
+  ElRow,
+  ElCol,
+  ElInput,
+  ElButton,
+  ElDatePicker,
+} from 'element-plus';
+
+export default defineComponent({
+  name: "start-page",
+  setup() {
+    const dateRange = ref<[number, number]>()
+    const textQuery = ref<string>()
+    return {dateRange, textQuery}
+  },
+  components: {
+    ElContainer,
+    ElBreadcrumb,
+    ElHeader,
+    ElRow,
+    ElCol,
+    ElInput,
+    ElButton,
+    ElDatePicker,
+  }
+});
+</script>
